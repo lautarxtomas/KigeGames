01c4d531dab25fd0405bb00185968d116b736c29
lautarxtomas <lautaro.martinez88@mi.unc.edu.ar>
2022-06-06 15:49:10 -0300

    agregando eventos al proyecto

--- js/app.js
+++ js/app.js
@@ -12,23 +12,24 @@ const videojuegos = []
 
 // CONSTRUCTOR
  class Videojuego {
-     constructor(nombre, consola, precio, genero, imgSrc) {
+     constructor(nombre, consola, precio, genero, imgSrc, id) {
          this.nombre = nombre
          this.consola = consola
          this.precio = precio
          this.genero = genero
          this.imgSrc = imgSrc
+         this.id = id
      }
  }
 
- const juego1 = videojuegos.push(new Videojuego ('Fifa 22', 'PS4', 6000, 'deportes', 'https://i.blogs.es/0a2b76/e53cynvwuaix9pa/450_1000.jpeg'))
- const juego2 = videojuegos.push(new Videojuego ('Persona 5', 'PS4', 4000, 'rpg', 'https://playtecgames.com/wp-content/uploads/2020/08/persona-5-royal.jpg'))
- const juego3 = videojuegos.push(new Videojuego ('God of War', 'PS4', 5000, 'accion', 'https://m.media-amazon.com/images/I/711lrrvzt+L._SL1496_.jpg'))
- const juego4 = videojuegos.push(new Videojuego ('Alien', 'PS4', 2000, 'terror', 'https://juegosdigitalesargentina.com/files/images/productos/1495070916-alien-isolation-the-collection-ps4.jpg'))
- const juego5 = videojuegos.push(new Videojuego ('PES 2021', 'PS4', 3000, 'deportes', 'https://www.portalgames.com.ar/wp-content/uploads/2020/09/pes-2021.jpg'))
+ const juego1 = videojuegos.push(new Videojuego ('Fifa 22', 'PS4', 6000, 'deportes', 'https://i.blogs.es/0a2b76/e53cynvwuaix9pa/450_1000.jpeg', 'F22'))
+ const juego2 = videojuegos.push(new Videojuego ('Persona 5', 'PS4', 4000, 'rpg', 'https://playtecgames.com/wp-content/uploads/2020/08/persona-5-royal.jpg', 'P5'))
+ const juego3 = videojuegos.push(new Videojuego ('God of War', 'PS4', 5000, 'accion', 'https://m.media-amazon.com/images/I/711lrrvzt+L._SL1496_.jpg', 'GOW4'))
+ const juego4 = videojuegos.push(new Videojuego ('Alien', 'PS4', 2000, 'terror', 'https://juegosdigitalesargentina.com/files/images/productos/1495070916-alien-isolation-the-collection-ps4.jpg', 'AIS'))
+ const juego5 = videojuegos.push(new Videojuego ('PES 2021', 'PS4', 3000, 'deportes', 'https://www.portalgames.com.ar/wp-content/uploads/2020/09/pes-2021.jpg', 'PES2021'))
 
 
-//  DOM
+//  ----------- DOM -----------
 
 const gameCards = document.getElementById('gameCards')
 
@@ -40,68 +41,96 @@ videojuegos.forEach((juego) => {
             <img src="${juego.imgSrc}" class="cardImg">
             <p class="cardDesc"> ${juego.consola} </p>
             <span class="cardPrice"> $${juego.precio} </span>
-            <button class="buttonCTA"> Agregar al Carrito </button>
+            <button data-id="${juego.id}" class="buttonCTA"> Agregar al Carrito </button>
         `
     gameCards.append(card)
 })
 
-  const carrito = []
-  const totalCarrito = () => {
-     let montoTotal = 0
-     carrito.forEach((videojuego) => {
-         montoTotal = montoTotal + videojuego.precio
-     })
-     return montoTotal
- }
- function descuento(total, descuento) {
-     let res = total - descuento
-     alert('Por esta semana usted obtiene un descuento de $' + descuento + ' al pasar los $8000. Su precio a pagar es de $' + res)
- }
- // FILTER
+const carrito = []
 
- const agregarJuego = () => {
-  let busqueda = prompt('Que videojuego estás buscando?:').toLowerCase();
-  const resultadoBusqueda = videojuegos.filter(
-      (videojuego) => videojuego.nombre.toLowerCase().includes(busqueda.toLowerCase())
-      );
+function descuento(total, descuento) {
+    let res = total - descuento
+    alert('Por esta semana usted obtiene un descuento de $' + descuento + ' al pasar los $8000. Su precio a pagar es de $' + res)
+}
 
-     //  Se crea un nuevo array con el juego buscado en caso de incluir el string ingresado en su nombre original. En este array temporal solo va a poder existir un juego por vuelta, por lo tanto, cuando lo agreguemos al carrito, va a ser usando de parametro resultadoBusqueda[0].
+const totalCarrito = () => {
+    let montoTotal = 0
+    carrito.forEach((videojuego) => {
+        montoTotal = montoTotal + videojuego.precio
+    })
+    if (montoTotal >= 8000){
+            descuento (montoTotal, 500)
+    } else {
+        alert('Su precio a pagar es de $' + montoTotal)
+    }
+    alert('MUCHAS GRACIAS POR SU COMPRA!')
+}
+    
+
+// EVENTOS
+
+const addGame = (e) => {
+   
+    const juegoElegido = e.target.getAttribute('data-id')
+    const videojuego = videojuegos.find((juego) => juego.id ==  juegoElegido)
+    carrito.push(videojuego)
+    console.log(carrito)
+}
+
+// BOTONES PARA AGREGAR JUEGOS AL CARRITO
+const botonesCompra = document.querySelectorAll('.buttonCTA')
+botonesCompra.forEach((botonCompra) => {
+    botonCompra.addEventListener('click', addGame)
+})
+
+
+// BOTON PARA FINALIZAR COMPRA
+const finalizarCompra = document.querySelectorAll('.finalizarCompra')
+finalizarCompra.forEach((botonFinalizar) => {
+    botonFinalizar.addEventListener('click', totalCarrito)
+})
+
+// ------------------------------------------------
+
+
+// FILTER
+
+const agregarJuego = () => {
+ let busqueda = prompt('Que videojuego estás buscando?:').toLowerCase();
+ const resultadoBusqueda = videojuegos.filter(
+     (videojuego) => videojuego.nombre.toLowerCase().includes(busqueda.toLowerCase())
+     );
+
+ //  Se crea un nuevo array con el juego buscado en caso de incluir el string ingresado en su nombre original. En este array temporal solo va a poder existir un juego por vuelta, por lo tanto, cuando lo agreguemos al carrito, va a ser usando de parametro resultadoBusqueda[0].
      
-     // Este if indica que si en el array creado con el filter hay por lo menos un juego, agregue el mismo al carrito
-     if (resultadoBusqueda.length === 1){
-      agregarAlCarrito(resultadoBusqueda[0]);
-     } else {
-         alert ('No tenemos ese videojuego o no lo buscó correctamente. Ingrese otro:')
-         agregarJuego()
-     }
- }
-
- const agregarAlCarrito = (videojuego) => {
-     alert(`Nombre: ${videojuego.nombre} \nPrecio: ${videojuego.precio}`);
-
-     if (confirm(`¿Desea agregar ${videojuego.nombre} al carrito?`)) {
-
-       carrito.push(videojuego);
-       if (confirm("Desea agregar otro juego?")) {
-
-         agregarJuego()
-
+// Este if indica que si en el array creado con el filter hay por lo menos un juego, agregue el mismo al carrito
+       if (resultadoBusqueda.length === 1){
+        agregarAlCarrito(resultadoBusqueda[0]);
        } else {
-         if (totalCarrito() >= 8000){
-             descuento (totalCarrito(), 500)
-         } 
-         else{
-         alert('Su compra ha sido realizada. Su monto total a pagar es de $' + totalCarrito())
-         }
-          
-             console.log(carrito)
-             alert('MUCHAS GRACIAS POR TU COMPRA! VUELVA PRONTO!')
+           alert ('No tenemos ese videojuego o no lo buscó correctamente. Ingrese otro:')
+           agregarJuego()
        }
-     } else {
-         agregarJuego()
-     }     
+   }
 
- }
+      const agregarAlCarrito = (videojuego) => {
+          alert(`Nombre: ${videojuego.nombre} \nPrecio: ${videojuego.precio}`);
+
+       if (confirm(`¿Desea agregar ${videojuego.nombre} al carrito?`)) {
+
+         carrito.push(videojuego);
+         if (confirm("Desea agregar otro juego?")) {
+
+           agregarJuego()
+
+         } else {
+              alert('Puede seguir agregando juegos al carrito o apretar el botón de finalizar compra para ver su monto total')
+              console.log(carrito)
+        }
+      } else {
+          agregarJuego()
+      }     
+
+   }
 
 
- agregarJuego()
+   agregarJuego()
